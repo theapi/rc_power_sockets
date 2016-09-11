8845876ae79dcba935e47149e6ef275360a0123b
theapi <petermclarke@gmail.com>
2016-09-11 15:04:54 +0100

    Simpler timing

--- arduino/pir_mains_powered/pir_mains_powered.ino
+++ arduino/pir_mains_powered/pir_mains_powered.ino
@@ -18,9 +18,6 @@
 
 // RC Switch library form https://github.com/sui77/rc-switch
 #include <RCSwitch.h>
-#include <avr/sleep.h>    // Sleep Modes
-#include <avr/power.h>    // Power management
-#include <avr/wdt.h>      // Watchdog
 
 #define PIN_MOTION_IN 2
 
@@ -36,40 +33,23 @@
   #define PIN_STATUS 3
 #endif
 
-//#define WD_DO_STUFF 225 // How many watchdog interupts before doing real work: 225 * 8 / 60 = 30 minutes.
-//const int WD_DO_STUFF = 110; // ~15 minutes
-const int WD_DO_STUFF = 3; // ~24 seconds
-
-byte count = 0;
 byte num_transmissions = 2; // How many times to send the command.
 byte state = 0; // 0 = off, 1 = on
-
-volatile int wd_isr = WD_DO_STUFF; // Start in do stuff mode.
 
 
 RCSwitch mySwitch = RCSwitch();
 
-/**
- * Wake up by watchdog
- */
-ISR(WDT_vect) {
-  ++wd_isr;
-}
+unsigned long previousMillis = 0;
+const long interval = 30000; // How long to stay on
 
-/**
- * Wake up by motion stopped.
- */
-void isr_motion() { 
-  wd_isr = 0; // Reset the timer
-}
 
 void setup() {
   
   // Turn off everything but timers
   ADCSRA = 0;  // disable ADC
-  power_all_disable();
-  power_timer0_enable();
-  power_timer1_enable();
+  //power_all_disable();
+  //power_timer0_enable();
+  //power_timer1_enable();
   //power_timer2_enable();
   
   pinMode(PIN_MOTION_IN, INPUT);
@@ -81,9 +61,7 @@ void setup() {
   pinMode(PIN_POWER, INPUT);
  
   
-  digitalWrite(PIN_DEBUG, HIGH);
-
-  watchdog_setup();
+  digitalWrite(PIN_DEBUG, LOW);
 
   // Transmitter enable
   mySwitch.enableTransmit(PIN_RADIO_OUT);
@@ -91,19 +69,22 @@ void setup() {
 }
 
 void loop() {
-  // Tell watchdog all is ok.
-  wdt_reset();
+
+ unsigned long currentMillis = millis();
+ 
 
   if (digitalRead(PIN_MOTION_IN)) {
+    digitalWrite(PIN_DEBUG, HIGH);
+    
+    // reset the timer
+    previousMillis = currentMillis;
+    
     // Movement happening.
     if (state == 0) {
       // Previously there was no movement,
       // so change state and turn stuff on.
       state = 1;
 
-     
-
-      wd_isr = 0; // Reset the timer
 
       // Turn on the indicator leds
       digitalWrite(PIN_STATUS, HIGH);
@@ -111,89 +92,34 @@ void loop() {
       for (int i = 0; i < num_transmissions; i++) {
         mySwitch.switchOn(CODE_NUMBER, CHANNEL_NUMBER);
         // Allow time for transmission
-        delay(1000);
+        delay(2000);
       }
     }
-  } else if (wd_isr < WD_DO_STUFF) {
-    // Carry on sleeping.
-    goToSleep();
   } else {
-   if (!digitalRead(PIN_MOTION_IN)) {
+    digitalWrite(PIN_DEBUG, LOW);
+
+    if (currentMillis - previousMillis >= interval) {
+      previousMillis = currentMillis;
+      
       // No movement.
       if (state == 1) {
         // Previously there was movement,
         // so change state and turn stuff off.
         state = 0;
-
+  
         // Turn off the indicator leds
         digitalWrite(PIN_STATUS, LOW);
-
+  
         for (int i = 0; i <= num_transmissions; i++) {
           mySwitch.switchOff(CODE_NUMBER, CHANNEL_NUMBER);
-          // Allow time for transmission
-          delay(2000);
+          // Allow time for transmission & motion going high on transmission
+          delay(4000);
         }
       }
-   
     }
+    
   }
 
 }
 
-void goToSleep() {
-  digitalWrite(PIN_DEBUG, LOW);
-
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-  power_all_disable();
-
-  MCUSR = 0; // clear the reset register 
-  noInterrupts();           // timed sequence follows
-  attachInterrupt(0, isr_motion, FALLING);
-  sleep_enable();
-                      
-  #if defined( __AVR_ATtiny85__ )
-    // no brown out
-  #else
-    // turn off brown-out enable in software
-    MCUCR = bit (BODS) | bit (BODSE);
-    MCUCR = bit (BODS); 
-  #endif
-  
-  interrupts();             // guarantees next instruction executed
-  sleep_cpu();  
-  
-  // cancel sleep as a precaution
-  sleep_disable();  
-
-  power_timer0_enable();
-  power_timer1_enable();
-  //power_timer2_enable();
-  
-
-  //power_all_enable();
-
-  digitalWrite(PIN_DEBUG, HIGH);
-}
-
-/**
- * Watchdog to sleep for maximum time (8 seconds).
- */
-void watchdog_setup() {
-  // Clear any previous watchdog interupt
-  MCUSR = 0;
-
-  #if defined( __AVR_ATtiny85__ )
-    // allow changes, disable reset
-    WDTCR = bit (WDCE) | bit (WDE);
-    // set interrupt mode and an interval 
-    WDTCR = bit (WDIE) | bit (WDP3) | bit (WDP0);    // set WDIE, and 8 seconds delay
-  #else
-    // allow changes, disable reset
-    WDTCSR = bit (WDCE) | bit (WDE);
-    // set interrupt mode and an interval 
-    WDTCSR = bit (WDIE) | bit (WDP3) | bit (WDP0);    // set WDIE, and 8 seconds delay
-  #endif
-
-  wdt_reset();
-}
 
